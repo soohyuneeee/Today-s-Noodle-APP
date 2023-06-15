97104ce498651c49caaca1a104e03fb9be189b49
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-15 10:24:46 +0900

    {Add} 설정파일

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:intl/date_symbol_data_local.dart';
+// import 'package:intl/date_symbol_data_file.dart';
+import 'package:intl/intl.dart';
+import 'package:noodle/model/noodles.dart';
+
 import 'home.dart';
-void main() => runApp(MyApp());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(NoodleAdapter());
+  //intl 사용
+  Intl.defaultLocale = 'ko_KR';
+  await initializeDateFormatting();
+  runApp(MyApp());
+  await Future.delayed(const Duration(seconds: 3));
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'SpoqaHanSansNeo',
         brightness: Brightness.light,
         backgroundColor: Colors.white,
-        accentColorBrightness: Brightness.light,
         visualDensity: VisualDensity.adaptivePlatformDensity,
         primaryColor: Colors.blueGrey,
         primarySwatch: Colors.blueGrey,
